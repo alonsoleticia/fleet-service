8265e226165a22c917fcb9b39027088b03363695
Leticia Alonso <lalonso@gmv.com>
2025-02-01 21:57:45 +0100

    Updated satellite model

--- src/models/satelliteModel.js
+++ src/models/satelliteModel.js
@@ -3,12 +3,27 @@ const mongoose = require('mongoose');
 // Define the schema for a satellite
 const satelliteSchema = new mongoose.Schema({
   name: { type: String, required: true },  // Satellite name (required field)
-  type: { type: String, required: true },  // Type of satellite (required field)
-  launchDate: { type: Date, required: true },  // Launch date of the satellite (required field)
-  status: { type: String, enum: ['active', 'inactive'], default: 'active' }  // Status of the satellite, default is 'active'
-});
+  slug: {type: String, required: true }, // Long name of the satellite
+  status: { type: String, enum: ['active', 'inactive'], default: 'active' }, // Status of the satellite, default is 'active'
+  company: { type: String, default: null } , // FIXME: related to 'companies' model (or null)
+  createdBy: { type: String }, // FIXME: related to 'users' model
+  updatedBy: { type: String }, // FIXME: related to 'users' model
+  orbit: {
+    longitude: { type: Number, required: true },
+    latitude: { type: Number, default: 0 },
+    inclination: { type: Number, default: 0 },
+    height: { type: Number, default: 35786.063 } // Kms
+  }
+},{
+  timestamps: true  // This automatically adds the 'createdAt' and 'updatedAt' fields
+}
+);
 
 // Create the model
 const Satellite = mongoose.model('Satellite', satelliteSchema);
 
 module.exports = Satellite;  // Export the model to be used in other parts of the application
+
+/*
+  Disclaimer: there may be missing fields for the moment such as 'origin' (TBC if needed), 'beams' (also TBC). It may also be interesting to type the satellites (i.e. TRADITIONAL, FLEXIBLE).
+*/
