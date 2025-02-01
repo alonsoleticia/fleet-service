a58f199a058dedbc3d34a7adcd28327b48fb8234
Leticia Alonso <lalonso@gmv.com>
2025-02-01 11:53:39 +0100

    Este archivo define los endpoints para las operaciones CRUD

---
+++ src/routes/satelliteRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const satelliteController = require('../controllers/satelliteController');
+
+// Rutas para manejar satélites
+router.post('/', satelliteController.createSatellite);
+router.get('/', satelliteController.getAllSatellites);
+router.get('/:id', satelliteController.getSatelliteById);
+router.put('/:id', satelliteController.updateSatellite);
+router.delete('/:id', satelliteController.deleteSatellite);
+
+module.exports = router;
