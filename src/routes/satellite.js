fa02ccd0f309be14f98e0fbeefdd5e1621e4cacc
Leticia Alonso <lalonso@gmv.com>
2025-02-02 18:24:58 +0100

    Updated routes for coherency, supporting the search by name or ID

--- src/routes/satellite.js
+++ src/routes/satellite.js
@@ -5,8 +5,9 @@ const satelliteController = require('../controllers/satellite');
 // Routes to handle satellites
 router.post('/', satelliteController.createSatellite);  // Create a new satellite
 router.get('/', satelliteController.getAllSatellites);  // Get all satellites
-router.get('/:id', satelliteController.getSatelliteById);  // Get a satellite by ID
-router.put('/:id', satelliteController.updateSatellite);  // Update a satellite by ID
-router.delete('/:id', satelliteController.deleteSatellite);  // Delete a satellite by ID
+router.get('/id/:id', satelliteController.getSatelliteById);  // Get a satellite by ID
+router.get('/name/:name', satelliteController.getSatelliteByName);  // Get a satellite by name
+router.put('/id/:id', satelliteController.updateSatellite);  // Update a satellite by ID
+router.delete('/id/:id', satelliteController.deleteSatellite);  // Delete a satellite by ID
 
 module.exports = router;  // Export the router to be used in other parts of the application
