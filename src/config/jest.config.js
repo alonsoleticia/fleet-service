fc55e5373dc27a725789cd237fea59417d14362c
Leticia Alonso <lalonso@gmv.com>
2025-02-09 12:43:58 +0100

    Remove jest config from package.json and put all config together in the jest.config.js

--- src/config/jest.config.js
+++ src/config/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     testEnvironment: "node", // For Jest to use Node.js instead of JSDOM
+    setupFilesAfterEnv: ["./test/supertest/setupTestDB.js"] //Ensure running the Mongo in memory when launching Jest
   };
   
